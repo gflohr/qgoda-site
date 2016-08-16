61a8d9b5190291b9334caea8535b0d777e606b7c
Guido Flohr <guido.flohr@cantanea.com>
2016-08-16 09:16:13 +0300

    various minor changes for syntax highlighting

--- gulpfile.js
+++ gulpfile.js
@@ -56,8 +56,8 @@ var paths = {
         vendor: {
             src: [
                    'node_modules/jquery/dist/jquery.js',
-                   'node_modules/bootstrap/dist/js/bootstrap.js',
-                   'node_modules/tether/dist/js/tether.js'
+                   'node_modules/tether/dist/js/tether.js',
+                   'node_modules/bootstrap/dist/js/bootstrap.js'
                  ],
             dest: dest.jsdir
         }
