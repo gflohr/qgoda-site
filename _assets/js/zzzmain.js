deddba0154dcb3fb0313d0fda49ebff35cb8f0d0
Guido Flohr <guido.flohr@cantanea.com>
2017-11-13 21:41:47 +0200

    enable on-scroll animations for mobile

--- _assets/js/zzzmain.js
+++ _assets/js/zzzmain.js
@@ -1,18 +1,7 @@
 //WOW Scroll Spy
 var wow = new WOW({
     //disabled for mobile
-    mobile: false,
+    mobile: true,
     offset:       100
 });
 wow.init();
-
-//Contact Form
-
-$('#submit').click(function(){
-
-$.post("assets/php/send.php", $(".contact-form").serialize(),  function(response) {   
- $('#success').html(response);
-});
-return false;
-
-});
