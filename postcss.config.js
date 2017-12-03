ff8e3a71708cd74306bf95381b5efcdda2dc266b
Guido Flohr <guido.flohr@cantanea.com>
2017-12-03 20:51:06 +0200

    use font-magician

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ module.exports = {
         require('postcss-cssnext'),
         require('postcss-modules')({
           generateScopedName: '[name]__[local]___[hash:base64:5]',
-        })
+        }),
+        require('postcss-font-magician')()
     ]
 }
