681210a0c9155d568d6d4c297c98b3b373d24472
Guido Flohr <guido.flohr@cantanea.com>
2017-12-04 08:39:42 +0200

    added prismjs

--- _assets/index.js
+++ _assets/index.js
@@ -2,3 +2,23 @@ require('./scss/bootstrap.scss');
 require('./scss/font-awesome.scss');
 require('./scss/qgoda.scss');
 require('./scss/toc.scss');
+
+// Prism.
+var Prism = require('prismjs');
+
+// Prism languages.
+require('prismjs/components/prism-yaml');
+require('prismjs/components/prism-markdown');
+require('prismjs/components/prism-perl');
+require('prismjs/components/prism-bash');
+require('prismjs/components/prism-javascript');
+
+// Prism theme.
+require('prismjs/themes/prism-coy.css');
+
+// Prism plug-ins.
+require('prismjs/plugins/line-numbers/prism-line-numbers');
+require('prismjs/plugins/autolinker/prism-autolinker');
+require('prismjs/plugins/command-line/prism-command-line');
+require('prismjs/plugins/custom-class/prism-custom-class');
+Prism.plugins.customClass.map(require('prismjs/themes/prism-coy.css.json'));
